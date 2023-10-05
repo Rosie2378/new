64f2eafb04cb671d156662c75b4d01e394e60243
RozanaAlzhrani <rosiealzhrani@gmail.com>
2023-10-05 17:32:39 +0300

    Initial Commit

---
+++ new/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  new
+//
+//  Created by Rozana on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+         }.frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color("background"))
+         
+        }
+        
+    }
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ new/newApp.swift
@@ -0,0 +1,17 @@
+//
+//  newApp.swift
+//  new
+//
+//  Created by Rozana on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct newApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
